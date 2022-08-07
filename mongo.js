bcd44351fc6996e4c9d47a5b650d6c7f42704a4a
Julkimo <santeri.julkunen@gmail.com>
2022-08-07 14:29:13 +0300

    osa3c commit 1
    -tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+  
+const password = process.argv[2]
+
+const url = `mongodb+srv://mamelukkikala:${password}@cluster0.hivnr.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    console.log("phonebook: ")
+    Person.find( {} ).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+    })
+}
+else {
+    const personName = process.argv[3]
+    const personNumber = process.argv[4]
+
+    const person = new Person({
+    name: personName,
+    number: personNumber,
+    })
+
+    person.save().then(result => {
+    console.log('person saved to the phonebook')
+    })
+}
+mongoose.connection.close()
