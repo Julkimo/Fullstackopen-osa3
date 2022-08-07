31074da55626cfb27b39bdd222db65a6be4cd5ca
Julkimo <santeri.julkunen@gmail.com>
2022-08-07 16:16:39 +0300

    osa3c2 commit
    - tehtävät 3.13 ja 3.14 tehty

--- index.js
+++ index.js
@@ -4,45 +4,60 @@ const morgan = require('morgan')
 morgan('tiny')
 const cors = require('cors')
 
+require('dotenv').config()
+const Person = require('./models/person')
+
 app.use(express.json())
-app.use(express.static('build'))
+
 app.use(morgan())
+
 app.use(cors())
 
-let persons =
-[
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122",
-    }
-]
+app.use(express.static('build'))
 
-app.get('/', (req, res) =>
-{
+const generateId = () => {
+    return Math.round(Math.random() * 10000)
+}
+
+app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Phonebook!</h1>')
 })
   
-app.get('/api/persons', (req, res) =>
-{
-    res.json(persons)
-})
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number)
+    {
+        return response.status(400).json({
+            error: 'name, number or both missing' 
+        })
+    }
   
+    const person = new Person({
+      identity: generateId(),
+      name: body.name,
+      number: body.number
+    })
+
+    // Onko nimi jo listassa-tarkastus myohemmin
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+})
+
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.find({identity: request.params.id}).then(person => {
+      response.json(person)
+    })
+})
+
 app.get('/info', (req, res) =>
 {
     const amountOfPeople = persons.length
@@ -54,64 +69,7 @@ app.get('/info', (req, res) =>
     res.send(message)
 })
 
-app.get('/api/persons/:id', (request, response) =>
-{
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person)
-    {
-        response.json(person)
-    }
-    else
-    {
-        response.status(404).end()
-    }
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
-
-app.delete('/api/persons/:id', (request, response) =>
-{
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-})
-
-const generateId = () =>
-{
-    return Math.round(Math.random() * 10000)
-}
-
-app.post('/api/persons', (request, response) =>
-{
-    const body = request.body
-    console.log(body)
-
-    if (!body.name || !body.number)
-    {
-        return response.status(400).json({
-            error: 'name, number or both missing' 
-        })
-    }
-
-    if (persons.find(person => person.name === body.name) != null)
-    {
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-        })
-    }
-  
-    const person = {
-      id: generateId(),
-      name: body.name,
-      number: body.number
-    }
-    
-    persons = persons.concat(person)
-
-    response.json(person)
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
